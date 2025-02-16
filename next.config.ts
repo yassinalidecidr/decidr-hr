05f8c0fd42b7be54b31beb35e41a8c9fd31918ef
yas <yassin.ali@decidr.ai>
2025-02-17 01:55:15 +1100

    test

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname, "src"),
-    };
-    return config;
-  },
+  output: "export",
 };
 
 export default nextConfig;
